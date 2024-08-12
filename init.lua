82442b25d5d77da6b69c43f1c242908c5ce761d6
Alex <shabib@Alexs-MacBook-Pro-2.local>
2024-08-12 21:32:02 +0100

    added relative line numbers

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.wo.relativenumber = true
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
