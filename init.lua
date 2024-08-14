05005d5515c2c7c8c4089d41c9f6a75296877c25
Alex <shabib@Alexs-MacBook-Pro-2.local>
2024-08-14 17:25:32 +0100

    added neo-tree

--- init.lua
+++ init.lua
@@ -80,6 +80,16 @@ require("lazy").setup({
           desc = "Buffer Local Keymaps (which-key)",
         },
       },
+    },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      }
     }
   },
   -- Configure any other settings here. See the documentation for more details.
