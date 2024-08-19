dc9f58ee782b17938669f3ddb52f513142662697
Alex <alex.shabib.hoggett@proton.me>
2024-08-19 13:58:59 +0100

    dashboard changes

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -5,6 +5,17 @@ return {
   config = function()
     require('dashboard').setup {
       theme = 'hyper',
+      config = {
+        week_header = {
+          enable = true,  --boolean use a week header
+          concat = "أنا المُسافِرُ داخِلي وأنا المُحاصَرُ بالثُّنائيات"  --concat string after time string line
+        },
+        disable_move = true,  -- boolean default is false disable move key
+        shortcut = {
+          -- action can be a function type
+          { desc = "Files", group = 'DashboardHeader', key = 'z', action = 'Telescope find_files' },
+        },
+      }
     }
   end,
   dependencies = { {'nvim-tree/nvim-web-devicons'}}
