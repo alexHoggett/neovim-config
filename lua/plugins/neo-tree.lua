a93c741eea00d4f3597ad4504e67a093dfdfd4c8
Alex <alex.shabib.hoggett@proton.me>
2024-08-24 11:20:49 +0100

    added to neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,6 +6,33 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  }
+  },
+  config = function()
+    vim.fn.sign_define("DiagnosticSignError",
+      {text = " ", texthl = "DiagnosticSignError"})
+    vim.fn.sign_define("DiagnosticSignWarn",
+      {text = " ", texthl = "DiagnosticSignWarn"})
+    vim.fn.sign_define("DiagnosticSignInfo",
+      {text = " ", texthl = "DiagnosticSignInfo"})
+    vim.fn.sign_define("DiagnosticSignHint",
+      {text = "󰌵", texthl = "DiagnosticSignHint"})
+
+    require("neo-tree").setup({
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+        git_status = {
+          symbols = {
+            unstaged = "󰄱",
+            staged = "󰱒",
+          },
+        },
+      }
+    })
+  end
 }
 
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,11 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-vim.opt.ignorecase = true -- search case insensitive
-vim.opt.smartcase = true -- search matters if capital letter
-vim.opt.inccommand = "split" -- "for incsearch while sub
-
-
 vim.wo.relativenumber = true
 
 
